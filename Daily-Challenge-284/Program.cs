62f2142807a8de40a38699e22959eee81f706c03
encid <ryancav@gmail.com>
2016-12-23 07:13:17 -0500

    commit

---
+++ Daily-Challenge-284/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DailyChallenge284 {
+    class Program {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
